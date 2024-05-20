3b77ae30732c1131c8a53c95030734a0ec55f6e6
Vegard <vegard@sgjerde.net>
2024-05-20 20:35:31 +0200

    Copy tellstick config from addon config

--- run.sh
+++ run.sh
@@ -6,5 +6,6 @@ export TDM_MQTT_PORT="$(bashio::config 'mqtt_port')"
 export TDM_MQTT_USER="$(bashio::config 'mqtt_username')"
 export TDM_MQTT_PASS="$(bashio::config 'mqtt_password')"
 
+cp /config/tellstick.conf /etc/tellstick.conf
 
 /usr/bin/supervisord -c /etc/supervisord.conf
